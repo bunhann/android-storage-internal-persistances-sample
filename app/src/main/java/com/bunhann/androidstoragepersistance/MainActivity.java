d92c323b01b8d7c725c8ebf5cc5478fb68fbde1c
Apple <apple@Apples-MacBook-Pro-2.local>
2019-05-21 17:42:16 +0700

    first commit

---
+++ app/src/main/java/com/bunhann/androidstoragepersistance/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bunhann.androidstoragepersistance;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnDownload, btnPlay;
+    private EditText edURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDownload = (Button) findViewById(R.id.btnDownload);
+        btnPlay = (Button) findViewById(R.id.btnViewFile);
+
+
+
+    }
+
+    private boolean isConnectingToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager
+                .getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+
+}
